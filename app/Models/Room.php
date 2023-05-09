498499ee37fff698a0693aa06927d8def649be03
Guido Ortiz <guidoortiz93@hotmail.com>
2023-05-09 09:48:36 +0200

    room model spelling

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Contact extends Model
+class Rooms extends Model
 {
     use HasFactory;
+
+    protected $table = "Rooms";
 }
